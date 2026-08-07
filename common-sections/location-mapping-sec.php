a8c3f72601f069fdca021eda39be19433cb5af19
wpdeveloper 2 <dhruv@karmaasource.com>
2026-08-07 15:09:43 +0530

    Hours update in local maps

--- common-sections/location-mapping-sec.php
+++ common-sections/location-mapping-sec.php
@@ -13,9 +13,14 @@
               <label class="rv-acc-head" for="loc-whitby-main">Whitby, Main Office &amp; Showroom</label>
               <h3>Whitby, Main Office &amp; Showroom</h3>
               <p class="location-sub">All services: doors, hardware, garage doors, fabrication</p>
-              <div class="location-map"><iframe title="Map to 111 Industrial Drive, Whitby"
+              <div class="location-map">
+                <!-- <iframe title="Map to 111 Industrial Drive, Whitby"
                   src="https://maps.google.com/maps?q=111%20Industrial%20Drive%20Whitby%20ON&t=&z=14&ie=UTF8&iwloc=&output=embed"
-                  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                
+                  <iframe title="Map to 111 Industrial Drive, Whitby" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1710.1981079125715!2d-78.92177845920924!3d43.87243008244536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51dd593bdb04b%3A0x184857d2dc5f33b1!2sRivett%20Architectural%20Hardware!5e0!3m2!1sen!2sin!4v1786080451498!5m2!1sen!2sin"  loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+
+                </div>
               <p class="location-line adress">
                 <img src="./src/images/adress-location-icon.webp" alt="" width="24" height="24" class="img-fluid">
                 111 Industrial Drive, Unit 1<br>Whitby,
@@ -30,12 +35,12 @@
                 <span class="main-time">
                   <span class="new-day-time">
                     <span class="day">Mon – Thu</span>
-                    <span class="jem">:</span>
+                    <!-- <span class="jem">:</span> -->
                     <span class="time">8:00 AM – 5:00 PM</span>
                   </span>
                   <span class="new-day-time">
                     <span class="day">Friday</span>
-                    <span class="jem">:</span>
+                    <!-- <span class="jem">:</span> -->
                     <span class="time">8:00 AM – 1:00 PM</span>
                   </span>
                 </span>
@@ -49,7 +54,7 @@
               <h3>Whitby, Overhead Doors Division</h3>
               <p class="location-sub">Residential &amp; commercial overhead doors, service dispatch</p>
               <div class="location-map">
-                <iframe title="Map to 105 Industrial Drive, Whitby" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2876.2192559637383!2d-78.92199852336114!3d43.87200863825076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51de50922046b%3A0x590c10eb0ec22bea!2s%231a%2C%20105%20Industrial%20Dr%20%236%2C%20Whitby%2C%20ON%20L1N%205Z9%2C%20Canada!5e0!3m2!1sen!2sin!4v1785740988098!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                <iframe title="Map to 105 Industrial Drive, Whitby" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2876.219441200941!2d-78.9194236!3d43.87200479999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51de50922046b%3A0x590c10eb0ec22bea!2s%231a%2C%20105%20Industrial%20Dr%20%236%2C%20Whitby%2C%20ON%20L1N%205Z9%2C%20Canada!5e0!3m2!1sen!2sin!4v1785995116797!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
                 
                 </div>
               <p class="location-line adress">
@@ -66,12 +71,12 @@
                 <span class="main-time">
                   <span class="new-day-time">
                     <span class="day">Mon – Thu</span>
-                    <span class="jem">:</span>
+                    <!-- <span class="jem">:</span> -->
                     <span class="time">8:00 AM – 5:00 PM</span>
                   </span>
                   <span class="new-day-time">
                     <span class="day">Friday</span>
-                    <span class="jem">:</span>
+                    <!-- <span class="jem">:</span> -->
                     <span class="time">8:00 AM – 4:00 PM</span>
                   </span>
                 </span>
@@ -101,12 +106,12 @@
                 <span class="main-time">
                   <span class="new-day-time">
                     <span class="day">Mon – Thu</span>
-                    <span class="jem">:</span>
+                    <!-- <span class="jem">:</span> -->
                     <span class="time">8:00 AM – 5:00 PM</span>
                   </span>
                   <span class="new-day-time">
                     <span class="day">Friday</span>
-                    <span class="jem">:</span>
+                    <!-- <span class="jem">:</span> -->
                     <span class="time">8:00 AM – 1:00 PM</span>
                   </span>
                 </span>
